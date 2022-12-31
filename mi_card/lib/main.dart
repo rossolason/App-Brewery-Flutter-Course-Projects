8c1005ec3ed33af022b3b72887c8a52ee5477fb3
Ross Olason <rossolason@gmail.com>
2022-12-31 14:47:20 -0800

    Color_Change

--- mi_card/lib/main.dart
+++ mi_card/lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.green,
         body: Container(),
       ),
     );
