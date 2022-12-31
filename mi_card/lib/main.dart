2b3a69efd724cf2fad0f28f022676f4d3e3eac94
Ross Olason <rossolason@gmail.com>
2022-12-31 15:09:12 -0800

    git-ignore

--- mi_card/lib/main.dart
+++ mi_card/lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.pink,
+        backgroundColor: Colors.red,
         body: Container(),
       ),
     );
