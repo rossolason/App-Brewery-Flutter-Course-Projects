0bb65465bd841b8dd72a2660bd9e09f135c8fb12
Ross Olason <rossolason@gmail.com>
2023-01-15 14:32:21 -0800

    Dice_Layout

--- dicee/lib/main.dart
+++ dicee/lib/main.dart
@@ -1,22 +1,46 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  return runApp(
-    MaterialApp(
+  return runApp(MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.red,
-        appBar: AppBar(
-          title: const Text('Dicee'),
-          backgroundColor: Colors.red,
-        ),
-      )
-    )
-  )
+    backgroundColor: Colors.red,
+    appBar: AppBar(
+      centerTitle: true,
+      title: const Text('Dicmee'),
+      backgroundColor: Colors.red,
+    ),
+    body: DicePage(),
+  )));
 }
 
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextButton(
+                onPressed: () {
+                  print('left');
+                },
+                child: Image.asset('images/dice1.png'),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextButton(
+                onPressed: null,
+                child: Image.asset('images/dice1.png'),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
