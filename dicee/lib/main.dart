ac86ac6ad5dd47f6946a69a646e918984340f87f
Ross Olason <rossolason@gmail.com>
2023-01-18 17:24:11 -0800

    Dice_Functionality

--- dicee/lib/main.dart
+++ dicee/lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -13,10 +14,24 @@ void main() {
   )));
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
+  void updateDice() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    var leftDiceNumber = 4;
     return Center(
       child: Row(
         children: <Widget>[
@@ -25,7 +40,7 @@ class DicePage extends StatelessWidget {
               padding: const EdgeInsets.all(8.0),
               child: TextButton(
                 onPressed: () {
-                  print('left');
+                  updateDice();
                 },
                 child: Image.asset('images/dice$leftDiceNumber.png'),
               ),
@@ -35,8 +50,10 @@ class DicePage extends StatelessWidget {
             child: Padding(
               padding: const EdgeInsets.all(8.0),
               child: TextButton(
-                onPressed: null,
-                child: Image.asset('images/dice1.png'),
+                onPressed: () {
+                  updateDice();
+                },
+                child: Image.asset('images/dice$rightDiceNumber.png'),
               ),
             ),
           ),
